2ffa002655aba443ed7370212e0f9842f70ca4a5
Igor Tsentylo <itsentylo@luxoft.com>
2021-03-25 22:48:09 +0200

    Create build-docker-ci.sh

---
+++ scripts/build-docker-ci.sh
@@ -0,0 +1,18 @@
+mkdir temp
+
+cp -v package.json yarn.lock lerna.json tsconfig.json .eslintrc.js .npmrc temp/
+cp -v ./scripts/create-npmrc.sh temp/
+find ./packages -name 'package.json' -exec sh -c 'cd ./temp && mkdir -p "${0%/*}" &&  cp -v "../$0" "${0}"' {} \; 
+
+IMAGE_NAME=$1
+BUILD_ENV_PARAM=$2
+NPM_CREDENTIALS=$3
+
+docker build \
+  --tag $IMAGE_NAME \
+  --build-arg BUILD_ENV_PARAM=$BUILD_ENV_PARAM \
+  --build-arg NPM_CREDENTIALS=$NPM_CREDENTIALS \
+  --file dockerfiles/Dockerfile \
+  .;
+
+rm -rf temp
