c45ff61a47f9f4ca951d74ef9fb3641b429fcd76
Ruslan Sapizhuk <ruslan.sapizhuk@gmail.com>
2021-03-31 15:10:30 +0300

    fix(client): TESCO-0 fix types

---
+++ packages/client/src/types/beans-accordion.d.ts
@@ -0,0 +1,7 @@
+declare module '@beans/accordion' {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  declare const Accordion: React.FC<any>;
+  declare const AccordionGroup: React.FC<any>;
+
+  export default { Accordion, AccordionGroup };
+}
