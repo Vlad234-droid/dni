22916568a78bdb88cb9ce90ddb3fc4d80831789d
Kateryna Kukunina <kkukunina@MacBook-Pro-Kateryna.local>
2021-05-14 15:27:27 +0300

    feat(client): TESCO-725 add useParams

--- packages/client/src/features/Page/components/NetworkNews/NetworkNews.tsx
+++ packages/client/src/features/Page/components/NetworkNews/NetworkNews.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+import { useParams } from 'react-router-dom';
 
 import { Page, PAGE_PREFIX } from 'features/Page';
 import { PostList, PostSingle } from 'features/Post';
@@ -10,7 +11,7 @@ import { Container, Left, Right } from './styled';
 
 const NetworkNews: FC = () => {
   const { isDesktop } = useMedia();
-  const postId = Number(window.location.pathname.split('/').pop());
+  const { id } = useParams<{ id: string }>();
 
   return (
     <div data-testid={`${PAGE_PREFIX}${Page.NETWORK_NEWS}`}>
@@ -18,7 +19,7 @@ const NetworkNews: FC = () => {
         renderMain={() => (
           <Container>
             <Left data-testid='container_feeds'>
-              {postId ? <PostSingle postId={postId} /> : <PostList />}
+              {id ? <PostSingle postId={parseInt(id, 10)} /> : <PostList />}
             </Left>
             {isDesktop && (
               <Right data-testid='container_sidebar'>
