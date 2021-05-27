c87fca3408815100521898f859da246901f793fb
Igor Tsentylo <igor.tsentilo@tesco.com>
2021-05-27 17:06:36 +0300

    TESCO-000 Modify WS URL calc. logic

--- codebase/packages/client/src/config/notification.ts
+++ codebase/packages/client/src/config/notification.ts
@@ -7,17 +7,24 @@ if (PUBLIC_URL && PUBLIC_URL !== '/') {
   socketUrl = `${PUBLIC_URL}${socketUrl}`;
 }
 
+console.log(`PUBLIC_URL = ${PUBLIC_URL}`);
+console.log(`SOCKET_URL = ${SOCKET_URL}`);
+console.log(`socketUrl = ${socketUrl}`);
+
+
 const defaultProps = { transports: ['websocket', 'polling'] };
 
 const NOTIFICATIONS = 'notifications';
 const NOTIFICATION_CREATE = 'notification-create';
 const NOTIFICATION_REMOVE = 'notification-remove';
 
-const socket = io(socketUrl!, defaultProps);
+const socket = io(socketUrl, defaultProps);
+
+console.log(socket);
 
 export {
   socket,
-  socketUrl,
+  //socketUrl,
   NOTIFICATIONS,
   NOTIFICATION_REMOVE,
   NOTIFICATION_CREATE,
