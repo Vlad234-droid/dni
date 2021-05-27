d2f5f4befc605dfb7c84040b3a3d89fc1b393f8d
Igor Tsentylo <igor.tsentilo@tesco.com>
2021-05-27 15:02:54 +0300

    TESCO-000 Fix docker file for client app build

--- codebase/packages/client/src/utils/history.ts
+++ codebase/packages/client/src/utils/history.ts
@@ -1,6 +1,6 @@
 import { createBrowserHistory as createHistory } from 'history';
 
-const history = createHistory();
+const history = createHistory({ basename: process.env.PUBLIC_URL });
 
 export type History = typeof history;
 
