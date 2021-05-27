b4b946614701485c2a81c930be7808cf8900f3b1
Igor Tsentylo <igor.tsentilo@tesco.com>
2021-05-27 18:59:58 +0300

    TESCO-000 Change docker image build script for Windows

--- scripts/build-docker.cmd
+++ scripts/build-docker.cmd
@@ -18,6 +18,8 @@ docker build ^
    --tag dni_local:latest ^
    --build-arg NEXUS_ACCESS_TOKEN=%NEXUS_ACCESS_TOKEN% ^
    --build-arg NODE_ENV=ppe ^
+   --build-arg REACT_APP_API_URL=/api ^
+   --build-arg REACT_APP_WS_URL=/socket.io ^
    --build-arg PUBLIC_URL=/ ^
    --file dockerfiles/dni-frontend_docker_build.Dockerfile ^
    .
