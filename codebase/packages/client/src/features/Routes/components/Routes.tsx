101afb55a98d1460daa6e9baf88f5db43e742b89
Kateryna Kukunina <kkukunina@MacBook-Pro-Kateryna.local>
2021-06-16 16:11:05 +0300

    feat(client): TESCO-910 leave filter routes by permissions

--- codebase/packages/client/src/features/Routes/components/Routes.tsx
+++ codebase/packages/client/src/features/Routes/components/Routes.tsx
@@ -1,16 +1,23 @@
 import React, { FC } from 'react';
 import { Route as ReactRoute, Switch, Redirect } from 'react-router';
 
+import { usePermission } from 'features/Auth';
+import { Action, buildAction } from 'features/Action';
 import { routes } from '../config/routes';
 
-// TODO: filter routes to render only available by role
-const Routes: FC = () => (
-  <Switch>
-    {routes.map(({ path, exact, Component }) => (
-      <ReactRoute key={path} path={path} exact={exact} component={Component} />
-    ))}
-    <Redirect from='*' to='/404' />
-  </Switch>
-);
+const Routes: FC = () => {
+  return (
+    <Switch>
+      {routes.map(({ page, path, exact, Component }) => {
+        const hasPermission = usePermission(buildAction(page, Action.VISIT));
+
+        if (hasPermission) {
+          return <ReactRoute key={path} path={path} exact={exact} component={Component} />;
+        }
+      })}
+      <Redirect from='*' to='/404' />
+    </Switch>
+  );
+};
 
 export default Routes;
