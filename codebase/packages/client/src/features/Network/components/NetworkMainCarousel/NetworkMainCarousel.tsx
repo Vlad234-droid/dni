ce7d306e239e0021a6a7bc09e065e53807b04257
Kateryna Kukunina <kukurina.ekaterina@tesco.com>
2021-06-18 15:49:31 +0300

    fix(client): TESCO-949 add force play

--- codebase/packages/client/src/features/Network/components/NetworkMainCarousel/NetworkMainCarousel.tsx
+++ codebase/packages/client/src/features/Network/components/NetworkMainCarousel/NetworkMainCarousel.tsx
@@ -6,20 +6,30 @@ import networks from '../../networks';
 
 const NetworkCarousel: FC = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [play, setPlay] = useState(!isOpen);
+  const [play, setPlay] = useState(true);
+  const [forcePlay, setForcePlay] = useState(!isOpen);
 
   const handleButtonClick = () => {
     setIsOpen(!isOpen);
+    !isOpen && setForcePlay(false);
+    isOpen && setForcePlay(true);
   };
 
-  const handlePlayClick = () => setPlay(!play);
+  const handleControlClick = () => {
+    isOpen && setIsOpen(false);
+  };
+
+  const handlePlayClick = () => {
+    setPlay(!play);
+  };
 
   return (
     <MainCarousel
       id='networks-preview-carousel'
       hideControls={false}
-      autoPlay={play}
+      autoPlay={isOpen ? forcePlay : play}
       onChange={handlePlayClick}
+      onControlClick={handleControlClick}
     >
       {networks.map(({ id, ...network }) => (
         <CarouselContent key={id} onButtonClick={handleButtonClick} isOpen={isOpen} {...network} />
