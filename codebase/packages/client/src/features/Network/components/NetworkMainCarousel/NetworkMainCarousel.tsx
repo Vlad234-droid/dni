c0a06d2a07920f0ff24746b93daff9daf13411b3
Kateryna Kukunina <kukurina.ekaterina@tesco.com>
2021-06-23 23:48:20 +0300

    fix(client): TESCO-949 fix desktop view

--- codebase/packages/client/src/features/Network/components/NetworkMainCarousel/NetworkMainCarousel.tsx
+++ codebase/packages/client/src/features/Network/components/NetworkMainCarousel/NetworkMainCarousel.tsx
@@ -1,20 +1,32 @@
 import React, { FC, useState, useEffect } from 'react';
+import styled from 'styled-components';
 
+import { useMedia } from 'context/InterfaceContext';
 import { CarouselContent } from 'features/MainCarousel';
 import Carousel from 'features/Carousel';
 
 import networks from '../../networks';
 
 const AUTO_SLIDE_INTERVAL = 10000;
+const MOBILE_ADDITION = 140;
 
 const NetworkCarousel: FC = () => {
+  const { isMobile, isLargeMobile } = useMedia();
   const [isOpen, setIsOpen] = useState(false);
   const [swipeHeight, setSwipeHeight] = useState<string>('auto');
-  const getChildHeight = (index: number) => document.getElementById(`carousel-content-${index}`)?.clientHeight;
+  const getChildHeight = (index: number) => {
+    const elHeight = document.getElementById(`carousel-content-${index}`)?.clientHeight;
+
+    if (isMobile || isLargeMobile) {
+      return elHeight! + MOBILE_ADDITION;
+    }
+
+    return elHeight;
+  };
   const [index, setIndex] = useState(0);
 
   useEffect(() => {
-    setSwipeHeight(isOpen ? `calc(${getChildHeight(index)}px + 140px)` : 'auto');
+    setSwipeHeight(isOpen ? `${getChildHeight(index)}px` : 'auto');
   }, [isOpen, index]);
 
   const handleButtonClick = (index: number) => {
@@ -27,20 +39,28 @@ const NetworkCarousel: FC = () => {
   }
 
   return (
-    <Carousel
-      id='networks-preview-carousel'
-      isOpen={isOpen}
-      onChange={handleOnChange}
-      interval={AUTO_SLIDE_INTERVAL}
-      height={swipeHeight}
-      fullWidth
-      continuous
-    >
-      {networks.map(({ id, ...network }, index) => (
-        <CarouselContent key={id} index={index} onButtonClick={() => handleButtonClick(index)} isOpen={isOpen} {...network} />
-      ))}
-    </Carousel>
+    <Wrapper>
+      <Carousel
+        id='networks-preview-carousel'
+        isOpen={isOpen}
+        onChange={handleOnChange}
+        interval={AUTO_SLIDE_INTERVAL}
+        height={swipeHeight}
+        fullWidth
+        continuous
+      >
+        {networks.map(({ id, ...network }, index) => (
+          <CarouselContent key={id} index={index} onButtonClick={() => handleButtonClick(index)} isOpen={isOpen} {...network} />
+        ))}
+      </Carousel>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  .swipe-item {
+    display: block;
+  }
+`;
 
 export default NetworkCarousel;
