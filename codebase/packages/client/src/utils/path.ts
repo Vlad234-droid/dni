81668fa0d9b313cc7e2871c484c1f6ec5cacf7a9
Igor Tsentylo <igor.tsentilo@tesco.com>
2021-07-03 23:42:02 +0300

    fix(client) TESCO-000: fix href in event ical file

--- codebase/packages/client/src/utils/path.ts
+++ codebase/packages/client/src/utils/path.ts
@@ -1,3 +1,8 @@
 import { PUBLIC_URL } from 'config/constants';
 
 export const getPath = (to: string) => (PUBLIC_URL === '/' ? `/${to}` : `/${PUBLIC_URL}/${to}`);
+
+export const getOrigin = () => {
+  const origin = window.location.origin;
+  return origin.endsWith('/') ? origin.slice(0, -1) : origin;
+}
