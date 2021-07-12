44e5fb92d0600eee0a609975003b6f8ec78d61ad
Igor Tsentylo <igor.tsentilo@tesco.com>
2021-07-13 01:02:17 +0300

    feat(server) TESCO-780: Update run.sh script.

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,18 +1,6 @@
 #!/bin/sh
 
 if [ "$TYPEORM_RUN_MIGRATION" = "true" ]; then
-
-    yarn ws:db typeorm query "DROP TABLE IF EXISTS \"public\".\"notification\" CASCADE;"
-    yarn ws:db typeorm query "DROP TABLE IF EXISTS \"public\".\"employee_history\" CASCADE;"
-    yarn ws:db typeorm query "DROP TABLE IF EXISTS \"public\".\"employee_network\" CASCADE;"
-    yarn ws:db typeorm query "DROP TABLE IF EXISTS \"public\".\"employee_event\" CASCADE;"
-    yarn ws:db typeorm query "DROP TYPE IF EXISTS \"public\".\"notification_entity_type_enum\" CASCADE;"
-    yarn ws:db typeorm query "DROP TYPE IF EXISTS \"public\".\"notification_action_enum\" CASCADE;"
-    yarn ws:db typeorm query "DROP TYPE IF EXISTS \"public\".\"employee_history_entity_type_enum\" CASCADE;"
-    yarn ws:db typeorm query "DROP TYPE IF EXISTS \"public\".\"employee_history_action_enum\" CASCADE;"
-
-    yarn ws:db typeorm query "DROP TABLE IF EXISTS \"public\".\"migrations\" CASCADE;"
-
     if [ "$TYPEORM_SCHEMA" != "" ]; then
         yarn ws:db typeorm query "CREATE SCHEMA IF NOT EXISTS \"$TYPEORM_SCHEMA\";" 
     fi
