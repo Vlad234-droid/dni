f2f4629617d11153ac020d7fa27e04795d935ada
Igor Tsentylo <igor.tsentilo@tesco.com>
2021-07-12 21:45:02 +0300

    feat(database) TESCO-780: Update run.sh to cleanup old tables

--- scripts/run.sh
+++ scripts/run.sh
@@ -11,6 +11,8 @@ if [ "$TYPEORM_RUN_MIGRATION" = "true" ]; then
     yarn ws:db typeorm query "DROP TYPE IF EXISTS \"public\".\"employee_history_entity_type_enum\" CASCADE;"
     yarn ws:db typeorm query "DROP TYPE IF EXISTS \"public\".\"employee_history_action_enum\" CASCADE;"
 
+    yarn ws:db typeorm query "DROP TABLE IF EXISTS \"public\".\"migrations\" CASCADE;"
+
     if [ "$TYPEORM_SCHEMA" != "" ]; then
         yarn ws:db typeorm query "CREATE SCHEMA IF NOT EXISTS \"$TYPEORM_SCHEMA\";" 
     fi
