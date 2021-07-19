1bbdbff8c7c88fbcb29b1221a3499e395c67f92d
Igor Tsentylo <igor.tsentilo@tesco.com>
2021-07-19 12:22:24 +0300

    feat(database) Add DB migration to update reporting funcs.

--- codebase/packages/database/src/migrations/sql/init_db_v1.1.sql
+++ codebase/packages/database/src/migrations/sql/init_db_v1.1.sql
@@ -569,6 +569,8 @@ BEGIN
       ),
       entity_hierarchy AS (SELECT
          ce.entity_instance,
+         ce.entity_type,
+         ce.entity_id,
          ce.parent_entity_type,
          ce.parent_entity_id,
          ra.root_ancestor_type,
@@ -576,23 +578,41 @@ BEGIN
       FROM root_ancestor ra
       JOIN ccms_entity ce ON ra.entity_id = ce.entity_id AND ra.entity_type = ce.entity_type
       WHERE ce.entity_published_at IS NOT NULL 
-        AND ce.entity_deleted_at IS NULL
+         AND ce.entity_deleted_at IS NULL
       ),
-      affected_users AS (SELECT
-         coalesce(jsonb_agg(dus.colleague_uuid), '[]'::jsonb) AS colleague_uuids
+      affected_users_1 AS (SELECT
+         dus.colleague_uuid AS colleague_uuid
       FROM params, dni_user_subscription dus 
       JOIN dni_user_extras due ON dus.colleague_uuid = due.colleague_uuid 
       WHERE 
          (
-            ((dus.subscription_entity_id IN (select parent_entity_id FROM entity_hierarchy)) AND (dus.subscription_entity_type IN (select parent_entity_type FROM entity_hierarchy))) OR 
-            ((dus.subscription_entity_id IN (select root_ancestor_id FROM entity_hierarchy)) AND (dus.subscription_entity_type IN (select root_ancestor_type FROM entity_hierarchy)))
+         ((dus.subscription_entity_id IN (select parent_entity_id FROM entity_hierarchy)) AND (dus.subscription_entity_type IN (select parent_entity_type FROM entity_hierarchy))) OR 
+         ((dus.subscription_entity_id IN (select root_ancestor_id FROM entity_hierarchy)) AND (dus.subscription_entity_type IN (select root_ancestor_type FROM entity_hierarchy)))
          )
          AND 
          (
-            (entity_type = 'post'::dni_entity_type_enum AND (due.settings->>'receivePostsEmailNotifications')::boolean = TRUE) OR 
-            (entity_type = 'event'::dni_entity_type_enum AND (due.settings->>'receiveEventsEmailNotifications')::boolean = TRUE) 
+         (entity_type = 'post'::dni_entity_type_enum AND (due.settings->>'receivePostsEmailNotifications')::boolean = TRUE) OR 
+         (entity_type = 'event'::dni_entity_type_enum AND (due.settings->>'receiveEventsEmailNotifications')::boolean = TRUE) 
          )
-      )
+      ),
+      affected_users_2 AS (SELECT
+         due.colleague_uuid AS colleague_uuid
+      FROM dni_user_extras due 
+      CROSS JOIN params
+      JOIN entity_hierarchy eh
+         ON params.entity_type = eh.entity_type AND params.entity_id = eh.entity_id
+      WHERE
+         ((params.entity_type = 'post'::dni_entity_type_enum AND (due.settings->>'receivePostsEmailNotifications')::boolean = TRUE) OR 
+         (params.entity_type = 'event'::dni_entity_type_enum AND (due.settings->>'receiveEventsEmailNotifications')::boolean = TRUE)) 
+         AND eh.parent_entity_type IS NULL AND eh.root_ancestor_type IS NULL
+      ),
+      affected_users AS (SELECT
+         coalesce(jsonb_agg(affected_users.colleague_uuid), '[]'::jsonb) AS colleague_uuids
+      FROM (
+         SELECT colleague_uuid FROM affected_users_1
+         UNION
+         SELECT colleague_uuid FROM affected_users_2
+      ) affected_users)
    SELECT 
       jsonb_build_object(
          'entity', entity_hierarchy.entity_instance,
