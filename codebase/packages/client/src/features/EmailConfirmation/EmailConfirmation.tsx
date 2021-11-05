f472a0de083060f41f4bf3d38c49a214a7b4d2fa
Kateryna Kukunina <kukurina.ekaterina@tesco.com>
2021-11-05 18:01:57 +0200

    feat(client): TESCO-1395 changes to EmailConfirmation

--- codebase/packages/client/src/features/EmailConfirmation/EmailConfirmation.tsx
+++ codebase/packages/client/src/features/EmailConfirmation/EmailConfirmation.tsx
@@ -1,25 +1,42 @@
 import React, { FC, useEffect, useMemo, useState } from 'react';
+import { Link } from 'react-router-dom';
+import Button from '@beans/button';
 
 import Loading from 'types/loading';
 import useFetch from 'hooks/useFetch';
+import { Spinner, Error } from 'features/Common';
+import useStore from 'hooks/useStore';
+import useDispatch from 'hooks/useDispatch';
+import { getPersonalEmail } from 'features/Notification';
+import { Page } from 'features/Page';
 
-import { Error, Spinner } from 'features/Common';
-import { Content, Wrapper } from './styled';
+import { Content, Wrapper, Email } from './styled';
 
 type Props = {
   token: string;
 };
 
 const EmailConfirmation: FC<Props> = ({ token }) => {
+  const dispatch = useDispatch();
+  const { personalEmail } = useStore((state) => state.notifications);
   const [success, setSuccess] = useState<boolean | null>(null);
 
-  const [{ response, loading, error }, doFetch] = useFetch<Event[]>([]);
+  const [{ loading, error }, doFetch] = useFetch<unknown>([]);
 
   const isLoading = useMemo(() => loading !== Loading.SUCCEEDED && loading !== Loading.FAILED, [loading]);
 
   useEffect(() => {
-    setSuccess(loading !== Loading.PENDING && !error);
-  }, [response, loading, error]);
+    dispatch(getPersonalEmail()); // remove
+
+    if (loading === Loading.SUCCEEDED) {
+      dispatch(getPersonalEmail());
+      setSuccess(true);
+    }
+
+    if (loading === Loading.FAILED) {
+      setSuccess(false);
+    }
+  }, [loading]);
 
   useEffect(() => {
     doFetch(
@@ -31,10 +48,23 @@ const EmailConfirmation: FC<Props> = ({ token }) => {
   const memoizedContent = useMemo(() => {
     if (error) return <Error errorData={{ title: 'Something went wrong', message: error }} />;
 
-    if (!response && isLoading) return <Spinner height='500px' />;
+    if (isLoading || !personalEmail) return <Spinner height='500px' />;
 
-    return <Content>{success ? 'Your email address has been successfully changed' : 'Something went wrong'}</Content>;
-  }, [success, error, response, isLoading]);
+    if (success) return (
+      <Content>Your email address has been successfully changed to&nbsp;
+        <Email>{personalEmail!.emailAddress}</Email>
+      </Content>
+    );
+
+    return (
+      <Content>
+        Email change was not completed. Please try resetting the email one more time.
+        <Link to={`/${Page.NOTIFICATION_SETTINGS}`}>
+          <Button>Settings</Button>
+        </Link>
+      </Content>
+    );
+  }, [success, error, isLoading, personalEmail]);
 
   return <Wrapper>{memoizedContent}</Wrapper>;
 };
--- codebase/packages/client/src/features/Toaster/components/ToasterSkinContent/ToasterSkinContent.tsx
+++ codebase/packages/client/src/features/Toaster/components/ToasterSkinContent/ToasterSkinContent.tsx
@@ -2,10 +2,11 @@ const ContentExample = () => <>Content example</>;
 const ContentErrorEntity = () => <>Something went wrong</>;
 const ContentSuccessEntity = () => <>Entity successfully created</>;
 const SettingsSuccess = () => <>Settings successfully saved</>;
-const ContentSuccessCopyLink = () => <>Link copied to clipboard.</>;
-const IntervalLimit = () => <>Period length should be less than one year.</>;
-const WrongInterval = () => <>Start date should be less than end date.</>;
-const EmailConfirmation = () => <>Email confirmation notification has been sent.</>;
+const ContentSuccessCopyLink = () => <>Link copied to clipboard</>;
+const IntervalLimit = () => <>Period length should be less than one year</>;
+const WrongInterval = () => <>Start date should be less than end date</>;
+const EmailConfirmationSuccess = () => <>Email confirmation notification has been sent</>;
+const EmailConfirmationError = () => <>Something went wrong</>;
 
 export {
   ContentExample,
@@ -15,5 +16,6 @@ export {
   ContentSuccessCopyLink,
   IntervalLimit,
   WrongInterval,
-  EmailConfirmation,
+  EmailConfirmationSuccess,
+  EmailConfirmationError,
 };
