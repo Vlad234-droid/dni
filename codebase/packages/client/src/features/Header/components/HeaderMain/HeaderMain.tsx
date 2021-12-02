d5f56e60f37f91ae6a755a5b90beff4526a474d4
Kateryna Kukunina <kukurina.ekaterina@tesco.com>
2021-12-02 16:44:01 +0200

    feat(client): TESCO-1446 update logo

--- codebase/packages/client/src/features/Header/components/HeaderMain/HeaderMain.tsx
+++ codebase/packages/client/src/features/Header/components/HeaderMain/HeaderMain.tsx
@@ -1,14 +1,20 @@
 import React, { FC } from 'react';
 
 import { MainMenuDesktop } from 'features/Menu';
+import { LINKS } from 'config/constants';
+import TescoLogo from '../../assets/christmas-tesco-logo.png';
 
-import { Logo } from './styled';
+import { Logo, LogoWrapper } from './styled';
 
 const TEST_ID = 'header-main';
 
 const HeaderMain: FC = () => (
-  <div data-testid='header-main'>
-    <Logo>Our Tesco</Logo>
+  <div data-testid={TEST_ID}>
+    <LogoWrapper>
+      <img src={TescoLogo} alt='Tesco logo' />
+    </LogoWrapper>
+
+    {/*<Logo>Our Tesco</Logo>*/}
     <MainMenuDesktop />
   </div>
 );
