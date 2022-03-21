9fa51982591eab31c21d3989bf453911a2634a9f
Igor Tsentylo <tsentylo@gmail.com>
2022-03-21 22:30:48 +0200

    feat/TESCO-000: Encrypt sensitive migration data

--- cicd/diversity-and-inclusion/prod/encrypt-secrets.cmd
+++ cicd/diversity-and-inclusion/prod/encrypt-secrets.cmd
@@ -6,5 +6,6 @@ rem az login --tenant f55b1f7d-7a7f-49e4-9b90-55218aad89f8
 sops ^
   --encrypt ^
   --azure-kv https://eun-prod-213-dai-gl.vault.azure.net/keys/diversity-and-inclusion-sops-key/84ae442c2470498a890991bdf40193b7 ^
-  secrets.json > secrets.enc.json
+  --output secrets.enc.json
+  secrets.json
     
