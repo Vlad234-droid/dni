07e34658d6bd2b560134f3d02589d633f6773453
Igor Tsentylo <tsentylo@gmail.com>
2022-04-11 17:02:25 +0300

    fix/DNI-000: Notification functions fixes (2)

--- codebase/packages/database/src/migrations/1649592000000-Update_bulk_mailing_functions.ts
+++ codebase/packages/database/src/migrations/1649592000000-Update_bulk_mailing_functions.ts
@@ -38,7 +38,7 @@ export class Update_bulk_mailing_functions implements MigrationInterface {
               , UNNEST(
                   CASE ce.entity_type::TEXT
                     WHEN 'post' THEN COALESCE(CASE WHEN ce.parent_event IS NULL THEN NULL ELSE ARRAY[ce.parent_event] end, ce.parent_networks)
-                    WHEN 'event' THEN ARRAY[ROW(ce.entity_id, ce.entity_type)::ccms_entity_descriptor]
+                    WHEN 'event' THEN ce.parent_networks
                     ELSE ARRAY[]::ccms_entity_descriptor[]
                   END
                 ) AS ancestor
@@ -147,6 +147,169 @@ export class Update_bulk_mailing_functions implements MigrationInterface {
         ;
       END
     `);
+
+    // -- =====================================
+    // -- fn_get_dni_user_notification_raw_list
+    // -- =====================================
+    await queryRunner.query(`
+      CREATE OR REPLACE FUNCTION fn_get_dni_user_notification_raw_list(
+          p_colleague_uuid UUID
+        , p_filter_entity_types dni_entity_type_enum[] DEFAULT ARRAY['event'::dni_entity_type_enum, 'post'::dni_entity_type_enum]
+        , p_filter_subscription_entity_types dni_entity_type_enum[] DEFAULT ARRAY['network'::dni_entity_type_enum]
+        , p_return_only_non_acknowledged BOOLEAN DEFAULT TRUE
+        , p_affected_interval INTERVAL DEFAULT '3 mons'::INTERVAL
+      )
+      RETURNS TABLE(
+          colleague_uuid uuid
+        , entity_type dni_entity_type_enum
+        , entity_id integer
+        , ancestors ccms_entity_descriptor[]
+        , ancestors_count integer
+        , notified_at timestamp with time zone
+        , acknowledged_at timestamp with time zone
+      )
+      LANGUAGE plpgsql
+      AS $function$
+      BEGIN
+        SET search_path TO "$user", dni, public;
+        
+        RETURN QUERY WITH params AS (
+          SELECT
+              p_colleague_uuid AS colleague_uuid
+            , p_filter_entity_types AS filter_entity_types
+            , p_filter_subscription_entity_types AS filter_subscription_entity_types
+            , p_return_only_non_acknowledged AS return_only_non_acknowledged
+            , p_affected_interval AS affected_interval
+        ),
+        entities_with_ancestors AS (
+          SELECT 
+              ce.entity_type
+            , ce.entity_id
+            , ce.entity_created_at
+            , ce.entity_updated_at
+            , ce.entity_published_at
+            , ce.entity_deleted_at
+            , COALESCE(ce.entity_updated_at, ce.entity_published_at) AS notified_at
+            , ARRAY_REMOVE(
+              CASE ce.entity_type::TEXT
+                WHEN 'post' THEN COALESCE(CASE WHEN ce.parent_event IS NULL THEN NULL ELSE ARRAY[ce.parent_event] end, ce.parent_networks)
+                WHEN 'event' THEN ce.parent_networks
+                --WHEN 'network' THEN ARRAY[ROW(ce.entity_id, ce.entity_type)::ccms_entity_descriptor]
+                ELSE ARRAY[]::ccms_entity_descriptor[]
+              END
+            , null) AS ancestors
+        FROM params, ccms_entity ce
+          LEFT JOIN ccms_entity pce
+          ON ce.parent_event = ROW(pce.entity_id, pce.entity_type)::ccms_entity_descriptor
+          WHERE ce.entity_type = ANY(params.filter_entity_types) 
+            AND ce.entity_deleted_at IS NULL 
+            AND ce.entity_published_at < CURRENT_TIMESTAMP 
+            AND COALESCE(ce.entity_updated_at, ce.entity_published_at) > CURRENT_DATE - params.affected_interval
+        ),
+        all_entities AS (
+          select 
+              entities_with_ancestors.entity_type
+            , entities_with_ancestors.entity_id
+            , entities_with_ancestors.entity_created_at
+            , entities_with_ancestors.entity_updated_at
+            , entities_with_ancestors.entity_published_at
+            , entities_with_ancestors.entity_deleted_at
+            , entities_with_ancestors.notified_at
+            , UNNEST(
+                CASE COALESCE(ARRAY_LENGTH(entities_with_ancestors.ancestors, 1), 0) 
+                WHEN 0 THEN ARRAY[NULL::ccms_entity_descriptor]
+                ELSE entities_with_ancestors.ancestors 
+                END
+              ) as ancestor
+          from entities_with_ancestors
+        ),
+        filtered_entities AS (
+          SELECT 
+              ae.entity_type
+            , ae.entity_id
+            , ae.entity_created_at
+            , ae.entity_updated_at
+            , ae.entity_published_at
+            , ae.entity_deleted_at
+            , ae.notified_at
+            , ae.ancestor AS ancestor
+          FROM params, all_entities ae
+          LEFT JOIN ccms_entity ace
+          ON ae.ancestor = ROW(ace.entity_id, ace.entity_type)::ccms_entity_descriptor
+          WHERE (ae.ancestor)."type" IS NULL 
+            OR ((ae.ancestor)."type" = ANY(params.filter_subscription_entity_types) AND ace.entity_published_at < CURRENT_TIMESTAMP)
+        ),
+        colleague_subsriptions AS (
+          SELECT
+              dus.colleague_uuid
+            , dus.subscription_entity_id AS entity_id
+            , dus.subscription_entity_type AS entity_type 
+          FROM params, dni_user_subscription dus 
+          WHERE dus.colleague_uuid = params.colleague_uuid
+            AND dus.subscription_entity_type = ANY(params.filter_subscription_entity_types)
+        ),
+        subscribed_colleague_entities AS (
+          SELECT 
+              fe.entity_type
+            , fe.entity_id
+            , fe.entity_created_at
+            , fe.entity_updated_at
+            , fe.entity_published_at
+            , fe.entity_deleted_at
+            , fe.notified_at
+            , fe.ancestor
+          FROM filtered_entities fe
+          LEFT JOIN colleague_subsriptions cs 
+          ON fe.ancestor = ROW(cs.entity_id, cs.entity_type)::ccms_entity_descriptor
+          WHERE (fe.ancestor)."id" IS NULL OR cs.colleague_uuid IS NOT NULL
+        ),
+        prepared_colleague_entities AS (
+          SELECT  
+              sce.entity_type
+            , sce.entity_id
+            , sce.entity_created_at
+            , sce.entity_updated_at
+            , sce.entity_published_at
+            , sce.entity_deleted_at
+            , MAX(sce.notified_at) AS notified_at
+            , ARRAY_AGG(ancestor) FILTER (WHERE (ancestor)."type" IS NOT NULL) AS ancestors
+          FROM params, subscribed_colleague_entities sce
+          GROUP BY
+              sce.entity_type
+            , sce.entity_id
+            , sce.entity_created_at
+            , sce.entity_updated_at
+            , sce.entity_published_at
+            , sce.entity_deleted_at
+        ),
+        acknowledged_colleague_entities AS (
+          SELECT 
+              a.acknowledge_entity_id AS acknowledged_entity_id
+            , a.acknowledge_entity_type AS acknowledged_entity_type
+            , MAX(a.acknowledge_created_at) AS acknowledged_at
+          FROM dni_user_notification_acknowledge a, params
+          WHERE a.colleague_uuid = params.colleague_uuid
+          GROUP BY 
+              a.colleague_uuid
+            , a.acknowledge_entity_id
+            , a.acknowledge_entity_type
+        )
+        SELECT
+            params.colleague_uuid AS colleague_uuid
+          , pe.entity_type AS entity_type
+          , pe.entity_id AS entity_id
+          , pe.ancestors AS ancestors
+          , COALESCE(array_length(pe.ancestors, 1), 0) AS ancestors_count
+          , pe.notified_at AS notified_at
+          , ack.acknowledged_at AS acknowledged_at
+        FROM params, prepared_colleague_entities pe
+        LEFT JOIN acknowledged_colleague_entities ack
+        ON pe.entity_id = ack.acknowledged_entity_id AND pe.entity_type = ack.acknowledged_entity_type
+        WHERE return_only_non_acknowledged = FALSE OR (ack.acknowledged_at IS NULL) 
+        ;
+      END
+      $function$
+    `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
@@ -154,6 +317,162 @@ export class Update_bulk_mailing_functions implements MigrationInterface {
     if (schema) {
       await queryRunner.query(`SET search_path TO "$user", ${schema}, public;`);
     }
+
+    // -- =====================================
+    // -- fn_get_dni_user_notification_raw_list
+    // -- =====================================
+    await queryRunner.query(`
+      CREATE OR REPLACE FUNCTION fn_get_dni_user_notification_raw_list(
+          p_colleague_uuid UUID
+        , p_filter_entity_types dni_entity_type_enum[] DEFAULT ARRAY['event'::dni_entity_type_enum, 'post'::dni_entity_type_enum]
+        , p_filter_subscription_entity_types dni_entity_type_enum[] DEFAULT ARRAY['network'::dni_entity_type_enum]
+        , p_return_only_non_acknowledged BOOLEAN DEFAULT TRUE
+        , p_affected_interval INTERVAL DEFAULT '3 mons'::INTERVAL
+      )
+      RETURNS TABLE(
+          colleague_uuid uuid
+        , entity_type dni_entity_type_enum
+        , entity_id integer
+        , ancestors ccms_entity_descriptor[]
+        , ancestors_count integer
+        , notified_at timestamp with time zone
+        , acknowledged_at timestamp with time zone
+      )
+      LANGUAGE plpgsql
+      AS $function$
+      BEGIN
+        SET search_path TO "$user", dni, public;
+        
+        RETURN QUERY WITH params AS (
+          SELECT
+              p_colleague_uuid AS colleague_uuid
+            , p_filter_entity_types AS filter_entity_types
+            , p_filter_subscription_entity_types AS filter_subscription_entity_types
+            , p_return_only_non_acknowledged AS return_only_non_acknowledged
+            , p_affected_interval AS affected_interval
+        ),
+        entities_with_ancestors AS (
+          SELECT 
+              ce.entity_type
+            , ce.entity_id
+            , ce.entity_created_at
+            , ce.entity_updated_at
+            , ce.entity_published_at
+            , ce.entity_deleted_at
+            , COALESCE(ce.entity_updated_at, ce.entity_published_at) AS notified_at
+            , ARRAY_REMOVE(pce.parent_networks || ce.parent_networks || ce.parent_event, null) AS ancestors
+          FROM params, ccms_entity ce
+          LEFT JOIN ccms_entity pce
+          ON ce.parent_event = ROW(pce.entity_id, pce.entity_type)::ccms_entity_descriptor
+          WHERE ce.entity_type = ANY(params.filter_entity_types) 
+            AND ce.entity_deleted_at IS NULL 
+            AND ce.entity_published_at < CURRENT_TIMESTAMP 
+            AND COALESCE(ce.entity_updated_at, ce.entity_published_at) > CURRENT_DATE - params.affected_interval
+        ),
+        all_entities AS (
+          select 
+              entities_with_ancestors.entity_type
+            , entities_with_ancestors.entity_id
+            , entities_with_ancestors.entity_created_at
+            , entities_with_ancestors.entity_updated_at
+            , entities_with_ancestors.entity_published_at
+            , entities_with_ancestors.entity_deleted_at
+            , entities_with_ancestors.notified_at
+            , UNNEST(
+                CASE COALESCE(ARRAY_LENGTH(entities_with_ancestors.ancestors, 1), 0) 
+                WHEN 0 THEN ARRAY[NULL::ccms_entity_descriptor]
+                ELSE entities_with_ancestors.ancestors 
+                END
+              ) as ancestor
+          from entities_with_ancestors
+        ),
+        filtered_entities AS (
+          SELECT 
+              ae.entity_type
+            , ae.entity_id
+            , ae.entity_created_at
+            , ae.entity_updated_at
+            , ae.entity_published_at
+            , ae.entity_deleted_at
+            , ae.notified_at
+            , ae.ancestor AS ancestor
+          FROM params, all_entities ae
+          LEFT JOIN ccms_entity ace
+          ON ae.ancestor = ROW(ace.entity_id, ace.entity_type)::ccms_entity_descriptor
+          WHERE (ae.ancestor)."type" IS NULL 
+            OR ((ae.ancestor)."type" = ANY(params.filter_subscription_entity_types) AND ace.entity_published_at < CURRENT_TIMESTAMP)
+        ),
+        colleague_subsriptions AS (
+          SELECT
+              dus.colleague_uuid
+            , dus.subscription_entity_id AS entity_id
+            , dus.subscription_entity_type AS entity_type 
+          FROM params, dni_user_subscription dus 
+          WHERE dus.colleague_uuid = params.colleague_uuid
+            AND dus.subscription_entity_type = ANY(params.filter_subscription_entity_types)
+        ),
+        subscribed_colleague_entities AS (
+          SELECT 
+              fe.entity_type
+            , fe.entity_id
+            , fe.entity_created_at
+            , fe.entity_updated_at
+            , fe.entity_published_at
+            , fe.entity_deleted_at
+            , fe.notified_at
+            , fe.ancestor
+          FROM filtered_entities fe
+          LEFT JOIN colleague_subsriptions cs 
+          ON fe.ancestor = ROW(cs.entity_id, cs.entity_type)::ccms_entity_descriptor
+          WHERE (fe.ancestor)."id" IS NULL OR cs.colleague_uuid IS NOT NULL
+        ),
+        prepared_colleague_entities AS (
+          SELECT  
+              sce.entity_type
+            , sce.entity_id
+            , sce.entity_created_at
+            , sce.entity_updated_at
+            , sce.entity_published_at
+            , sce.entity_deleted_at
+            , MAX(sce.notified_at) AS notified_at
+            , ARRAY_AGG(ancestor) FILTER (WHERE (ancestor)."type" IS NOT NULL) AS ancestors
+          FROM params, subscribed_colleague_entities sce
+          GROUP BY
+              sce.entity_type
+            , sce.entity_id
+            , sce.entity_created_at
+            , sce.entity_updated_at
+            , sce.entity_published_at
+            , sce.entity_deleted_at
+        ),
+        acknowledged_colleague_entities AS (
+          SELECT 
+              a.acknowledge_entity_id AS acknowledged_entity_id
+            , a.acknowledge_entity_type AS acknowledged_entity_type
+            , MAX(a.acknowledge_created_at) AS acknowledged_at
+          FROM dni_user_notification_acknowledge a, params
+          WHERE a.colleague_uuid = params.colleague_uuid
+          GROUP BY 
+              a.colleague_uuid
+            , a.acknowledge_entity_id
+            , a.acknowledge_entity_type
+        )
+        SELECT
+            params.colleague_uuid AS colleague_uuid
+          , pe.entity_type AS entity_type
+          , pe.entity_id AS entity_id
+          , pe.ancestors AS ancestors
+          , COALESCE(array_length(pe.ancestors, 1), 0) AS ancestors_count
+          , pe.notified_at AS notified_at
+          , ack.acknowledged_at AS acknowledged_at
+        FROM params, prepared_colleague_entities pe
+        LEFT JOIN acknowledged_colleague_entities ack
+        ON pe.entity_id = ack.acknowledged_entity_id AND pe.entity_type = ack.acknowledged_entity_type
+        WHERE return_only_non_acknowledged = FALSE OR (ack.acknowledged_at IS NULL) 
+        ;
+      END
+      $function$
+    `);
 
     // -- ============================
     // -- fn_get_dni_bulk_mailing_data
